6a46946059832baa3d1a31f71ae36c4fc77b9db5
Brandon <bmjohnson04@gmail.com>
2016-05-12 16:57:51 -0400

    created Articles save method

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160419181414) do
+ActiveRecord::Schema.define(version: 20160512203621) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "todo_items", force: :cascade do |t|
     t.string   "description"
