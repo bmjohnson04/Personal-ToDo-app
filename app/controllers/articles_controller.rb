34144f85e536fbc2b5b1b6ecb6b26249b1ea4e99
Brandon <bmjohnson04@gmail.com>
2016-05-13 18:57:54 -0400

    aded article prams to show method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,28 @@
 class ArticlesController < ApplicationController
+	def index
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
 	def new
 	end
 
+	def edit
+	end
+
 	def create
-		@article = Article.new(params[:article])
+		@article = Article.new(params.require(:article).permit(:title,:text))
 
 		@article.save
 		redirect_to @article
 	end
+
+	def update
+	end
+
+	def destroy
+	end
 end
+
